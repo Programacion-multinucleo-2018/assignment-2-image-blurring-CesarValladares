2d41f8b4a108ae3e88feb82614c357a8031dc0e9
CesarValladares <a01023506@itesm.mx>
2018-09-13 00:58:23 -0500

    tarea

---
+++ Blur.cu
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <cuda_runtime.h>
+#include <strings.h>
+#include "common.h"
+
+#define MS 5;
+
+using namespace std;
+using namespace cv;
+
+__global__ void Blur_Kernel(unsigned char* input, unsigned char* output, int width, int height, int colorWidthStep){
+
+    // 2D Index of current thread
+	const int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+    const int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int ref = floor(MS/2);
+    
+    if ((xIndex < width) && (yIndex < height))
+	{
+        int blue = 0; 
+        int green = 0; 
+        int red = 0;
+
+        int contador=0;
+
+        const int color_tid = (yIndex) * colorWidthStep + (3 * xIndex);
+
+        for(int i = -ref ; i <= ref ; i++){
+            for (int j = -ref; j<=ref ; j++){
+
+                //Location of colored pixel in input
+                const int color_tid = (yIndex+i) * colorWidthStep + (3 * (xIndex+j));
+                if(xIndex+j>0 && yIndex+i>0 && xIndex+j<width && yIndex+i<height ){
+                
+                    contador++;
+
+                    blue += input[color_tid];
+                    green += input[color_tid+1];
+                    red += input[color_tid+2];
+
+                }
+            }
+        }
+
+        output[color_tid] = static_cast<unsigned char>(blue/contador);
+		output[color_tid+1] = static_cast<unsigned char>(green/contador);
+        output[color_tid+2] = static_cast<unsigned char>(red/contador);   
+	}
+}
+
+void Blur(string file){
+
+    // Set up device
+    int dev = 0;
+    cudaDeviceProp deviceProp;
+    SAFE_CALL(cudaGetDeviceProperties(&deviceProp, dev), "Error device prop");
+    printf("Using Device %d: %s\n", dev, deviceProp.name);
+    SAFE_CALL(cudaSetDevice(dev), "Error setting device");
+
+    Mat input = cv::imread(file, CV_LOAD_IMAGE_COLOR);
+    cout << "Input image step: " << input.step << " rows: " << input.rows << " cols: " << input.cols << endl;
+
+    //Create output image
+    Mat output(input.rows, input.cols, CV_8UC3);
+    
+
+    // Calculate total number of bytes of input and output image
+	// Step = cols * number of colors	
+	size_t colorBytes = input.step * input.rows;
+    size_t grayBytes = output.step * output.rows;
+    
+    unsigned char *d_input, *d_output;
+
+    // Allocate device memory
+	SAFE_CALL(cudaMalloc<unsigned char>(&d_input, colorBytes), "CUDA Malloc Failed");
+	SAFE_CALL(cudaMalloc<unsigned char>(&d_output, grayBytes), "CUDA Malloc Failed");
+
+	// Copy data from OpenCV input image to device memory
+    SAFE_CALL(cudaMemcpy(d_input, input.ptr(), colorBytes, cudaMemcpyHostToDevice), "CUDA Memcpy Host To Device Failed");
+    SAFE_CALL(cudaMemcpy(d_output, output.ptr(), colorBytes, cudaMemcpyHostToDevice), "CUDA Memcpy Host To Device Failed");
+
+	// Specify a reasonable block size
+    const dim3 block(16, 16);
+    
+    // Calculate grid size to cover the whole image
+    const dim3 grid((int)ceil((float)input.cols / block.x), (int)ceil((float)input.rows/ block.y));
+    printf("Blur_Kernel<<<(%d, %d) , (%d, %d)>>>\n", grid.x, grid.y, block.x, block.y);
+    
+    // Launch the color conversion kernel
+    Blur_Kernel <<<grid, block >>>(d_input, d_output, input.cols, input.rows, static_cast<int>(input.step));
+    
+    // Synchronize to check for any kernel launch errors
+	SAFE_CALL(cudaDeviceSynchronize(), "Kernel Launch Failed");
+
+	// Copy back data from destination device meory to OpenCV output image
+	SAFE_CALL(cudaMemcpy(output.ptr(), d_output, grayBytes, cudaMemcpyDeviceToHost), "CUDA Memcpy Host To Device Failed");
+
+	// Free the device memory
+	SAFE_CALL(cudaFree(d_input), "CUDA Free Failed");
+	SAFE_CALL(cudaFree(d_output), "CUDA Free Failed");
+}
+
+
+int main(int argc, char *argv[]){
+
+    if (argc < 2){
+         
+        cout << "No hay argumentos suficientes" << endl;
+
+    }else{
+
+        Blur(argv[1]);
+
+    }
+
+}   
---
+++ CPUomp.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <chrono>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+#include <strings.h>
+
+#define MS 5;
+
+using namespace std;
+using namespace cv;
+
+void BlurImage(const Mat &input, Mat &output, int cols, int rows){
+
+    int ms = MS;
+    int ref = floor(ms/2);
+
+    int i, j, ii, jj;
+
+    #pragma omp parallel for private(i,j,ii,jj) shared(input, output)
+
+    for ( i = 0 ; i < cols ; i ++){
+        for ( j = 0 ; j < rows ; j++){
+
+            int blue = 0;
+            int green = 0;
+            int red = 0;
+
+            int contador = 0;
+
+            for ( ii = -ref ; ii <= ref ; ii++){
+                for ( jj = - ref ; jj <=ref ; jj++){
+
+                    if ( i + ii < 0 && j + jj < 0 && j + jj >= rows && i + ii >= cols ){
+
+                        //Casilla de la matriz aux fuera de la imagen
+                    }
+                    else
+                    {
+                        contador++;
+
+                        blue += input.at<Vec3b>(j+jj, i+ii)[0];
+                        green += input.at<Vec3b>(j+jj, i+ii)[1];
+                        red += input.at<Vec3b>(j+jj, i+ii)[2];
+
+                    }
+
+                }
+            }
+
+            output.at<Vec3b>(j,i)[0] = (blue/contador);
+            output.at<Vec3b>(j,i)[1] = (green/contador);
+            output.at<Vec3b>(j,i)[2] = (red/contador);            
+
+        }
+    }
+}
+
+void Blur(string file){
+
+    Mat image;
+
+    image = imread(file, CV_LOAD_IMAGE_COLOR);
+
+    if(!image.data)
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        
+    }else{
+
+        int x = image.cols;
+        int y = image.rows;
+
+        Mat output(image.rows, image.cols, CV_8UC3);
+
+        cout << "Input image step: " << image.step << " cols: " << x << " rows: " << y << endl;
+
+
+        BlurImage(image, output, x, y);
+
+        namedWindow("Original", cv::WINDOW_NORMAL);
+        namedWindow("Output", cv::WINDOW_NORMAL);
+
+        imshow("Original", image);
+        imshow("Output", output);
+    }
+
+}
+
+int main (int argc, char** argv){
+
+    if (argc < 2){
+         
+        cout << "No hay argumentos suficientes" << endl;
+
+    }else{
+
+        auto startTime = chrono::high_resolution_clock::now();
+        Blur(argv[1]);
+        auto endTime = chrono::high_resolution_clock::now();
+        chrono::duration<float, std::milli> duration_ms = endTime - startTime;
+
+        printf("Tiempo transcurrido:  %f ms \n", duration_ms.count());
+
+    }
+    waitKey(0); 
+
+    return 0;
+}
+
+
